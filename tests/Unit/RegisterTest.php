fec9e8a4aa08d827ef40228d8ecb667657efda37
Kevin Fuentes <fuenteskevin2015@gmail.com>
2020-08-05 15:58:25 +0800

    creates test case for register API taken email

--- tests/Unit/RegisterTest.php
+++ tests/Unit/RegisterTest.php
@@ -9,16 +9,37 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterTest extends TestCase
 {
-    public function test_can_register_user() {
+    public function testRegisterUser() {
         $data = [
             'email' => 'fuenteskevin2015@gmail.com',
-            'password' => 'adminadmin'
+            'password' => bcrypt('adminadmin')
         ];
 
         $this->json('POST', 'api.dishtansya.com/v1/register', $data, ['Accept' => 'application/json'])
             ->assertStatus(201)
             ->assertJsonStructure([
                 "message"
+            ]);
+    }
+
+    public function testEmailTaken() {
+        $user = User::create([
+            'email' => 'fuenteskevin2015@gmail.com',
+            'password' => bcrypt('adminadmin')
+        ]);
+
+        $data = [
+            'email' => 'fuenteskevin2015@gmail.com',
+            'password' => bcrypt('adminadmin')
+        ];
+
+        $this->json('POST', 'api.dishtansya.com/v1/register', $data, ['Accept' => 'application/json'])
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    'email' => ["The email has already been taken."]
+                ]
             ]);
     }
 }
